4f7298b74ac679038314db04a6c8a13a7df4eeaa
E1I4S <eliascardozo013@gmail.com>
2024-10-17 14:48:12 -0300

    Update tabla_anuncios.py

--- tabla_anuncios.py
+++ tabla_anuncios.py
@@ -1,5 +1,4 @@
 def solicitar_opcion():
-    """Solicita y devuelve una opción válida entre 1 y 3."""
     while True:
         print('Seleccione el proceso que desea aplicar')
         print('1: Ingresar puntuación y comentario')
@@ -12,7 +11,6 @@ def solicitar_opcion():
 
 
 def solicitar_puntuacion():
-    """Solicita y devuelve una puntuación válida entre 1 y 5."""
     while True:
         print('Por favor, introduzca una puntuación en una escala de 1 a 5')
         puntuacion = input()
@@ -24,21 +22,17 @@ def solicitar_puntuacion():
 
 
 def ingresar_puntuacion_y_comentario():
-    """Permite al usuario ingresar una puntuación y un comentario."""
     puntuacion = solicitar_puntuacion()
     print('Por favor, introduzca un comentario')
     comentario = input()
     post = f'Puntuación: {puntuacion} | Comentario: {comentario}'
     
-    # Guardar en archivo
     with open("data.txt", 'a') as file_pc:
         file_pc.write(f'{post}\n')
-    
     print('Comentario guardado exitosamente.')
 
 
 def comprobar_resultados():
-    """Muestra los resultados obtenidos hasta ahora."""
     try:
         with open("data.txt", 'r') as read_file:
             contenido = read_file.read()
@@ -52,10 +46,8 @@ def comprobar_resultados():
 
 
 def main():
-    """Controla el flujo principal del programa."""
     while True:
         opcion = solicitar_opcion()
-
         if opcion == 1:
             ingresar_puntuacion_y_comentario()
         elif opcion == 2:
@@ -67,5 +59,4 @@ def main():
             print('Por favor, introduzca un número del 1 al 3')
 
 
-# Ejecutar el programa
 main()
